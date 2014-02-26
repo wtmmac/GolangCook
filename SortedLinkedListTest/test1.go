ec018f5238409a48e0cdec488e2cc671b2653f3f
tonywang <tonywang@tonywangtekiMacBook-Pro.local>
2014-02-26 17:46:32 +0800

    up

--- SortedLinkedListTest/test1.go
+++ SortedLinkedListTest/test1.go
@@ -1,30 +1,69 @@
 package main
+
 import (
-    "fmt"
-    "GolangTest/SortedLinkedList"
+	"GolangTest/SortedLinkedList"
+	"danmuCache2"
+	"fmt"
 )
+
+var (
+	dmCache *danmuCache2.Cache
+)
+
 type WordCount struct {
-    Word  string
-    Count int
+	Word  string
+	Count int
 }
-func compareValue(old, new interface {}) bool {
-    if new.(WordCount).Count > old.(WordCount).Count {
-        return true
-    }
-    return false
+
+func compareValue(old, new interface{}) bool {
+	if new.(WordCount).Count > old.(WordCount).Count {
+		return true
+	}
+	return false
 }
+
 func main() {
-    wordCounts := []WordCount{
-        WordCount{"kate", 87},
-        WordCount{"herry", 92},
-        WordCount{"james", 81}}
+	dmCache = danmuCache2.New(0)
 
-    var aSortedLinkedList = SortedLinkedList.NewSortedLinkedList(10, compareValue)
+	wordCounts := []WordCount{
+		WordCount{"kate", 87},
+		WordCount{"herry", 92},
+		WordCount{"james", 81}}
 
-    for _, wordCount := range wordCounts {
-        aSortedLinkedList.PutOnTop(wordCount)
-    }
-    for element := aSortedLinkedList.List.Front(); element != nil; element = element.Next() {
-        fmt.Println(element.Value.(WordCount))
-    }
+	var aSortedLinkedList = SortedLinkedList.NewSortedLinkedList(10, compareValue)
+
+	for _, wordCount := range wordCounts {
+		aSortedLinkedList.PutOnTop(wordCount)
+	}
+	for element := aSortedLinkedList.List.Front(); element != nil; element = element.Next() {
+		fmt.Println(element.Value.(WordCount))
+	}
+
+	// set cache
+	id := "1"
+	danmu := make(danmuCache2.Danmu)
+
+	// 第一分钟
+	danmu["1"] = []danmuCache2.DanmuMinute{
+		danmuCache2.DanmuMinute{1, "测试", 1300002101},
+		danmuCache2.DanmuMinute{2, "测试", 1300002101},
+		danmuCache2.DanmuMinute{3, "测试", 1300002101}}
+
+	// 第二分钟
+	danmu["2"] = []danmuCache2.DanmuMinute{
+		danmuCache2.DanmuMinute{0, "测试", 1300002101},
+		danmuCache2.DanmuMinute{0, "测试", 1300002101},
+		danmuCache2.DanmuMinute{0, "测试", 1300002101}}
+
+	dmCache.Add(id, danmu)
+
+	// get cache
+	if v, ok := dmCache.Get("1"); ok {
+		// 返回弹幕
+		fmt.Println(v.String())
+		fmt.Println("==\t==")
+		fmt.Println(v["1"][1].Content)
+	} else {
+		fmt.Println("no value")
+	}
 }
