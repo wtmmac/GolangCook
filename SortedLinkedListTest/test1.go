c3d48b898de29062be612054b5da764cb64e9056
tonywang <tonywang@tonywangtekiMacBook-Pro.local>
2014-02-26 13:20:32 +0800

    up

---
+++ SortedLinkedListTest/test1.go
@@ -0,0 +1,30 @@
+package main
+import (
+    "fmt"
+    "GolangTest/SortedLinkedList"
+)
+type WordCount struct {
+    Word  string
+    Count int
+}
+func compareValue(old, new interface {}) bool {
+    if new.(WordCount).Count > old.(WordCount).Count {
+        return true
+    }
+    return false
+}
+func main() {
+    wordCounts := []WordCount{
+        WordCount{"kate", 87},
+        WordCount{"herry", 92},
+        WordCount{"james", 81}}
+
+    var aSortedLinkedList = SortedLinkedList.NewSortedLinkedList(10, compareValue)
+
+    for _, wordCount := range wordCounts {
+        aSortedLinkedList.PutOnTop(wordCount)
+    }
+    for element := aSortedLinkedList.List.Front(); element != nil; element = element.Next() {
+        fmt.Println(element.Value.(WordCount))
+    }
+}
