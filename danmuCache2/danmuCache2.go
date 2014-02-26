848199ae77dcd7c29ee8dc79670cb2cb55736a35
tonywang <tonywang@tonywangtekiMacBook-Pro.local>
2014-02-26 17:47:46 +0800

    danmu cache

---
+++ danmuCache2/danmuCache2.go
@@ -0,0 +1,139 @@
+/*
+Copyright 2013 Tudou Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package danmuCache 为土豆弹幕的缓存服务.
+package danmuCache2
+
+import "container/list"
+import "encoding/json"
+
+// Cache is an LRU cache. It is not safe for concurrent access.
+type Cache struct {
+	// MaxEntries is the maximum number of cache entries before
+	// an item is evicted. Zero means no limit.
+	MaxEntries int
+
+	// OnEvicted optionally specificies a callback function to be
+	// executed when an entry is purged from the cache.
+	OnEvicted func(key Key, value Danmu)
+
+	ll    *list.List
+	cache map[interface{}]*list.Element
+}
+
+// 弹幕分钟内每条数据的数据结构
+type DanmuMinute struct {
+	Zhiren    int
+	Content   string
+	Timestamp int
+}
+
+// 弹幕字典
+type Danmu map[string][]DanmuMinute
+
+// 弹幕Key
+type Key interface{}
+
+// 弹幕keyValue实体
+type entry struct {
+	key   Key
+	value Danmu
+}
+
+// 弹幕的String方法
+func (d Danmu) String() string {
+	slcB, _ := json.Marshal(d)
+	return string(slcB)
+}
+
+// New creates a new Cache.
+// If maxEntries is zero, the cache has no limit and it's assumed
+// that eviction is done by the caller.
+func New(maxEntries int) *Cache {
+	return &Cache{
+		MaxEntries: maxEntries,
+		ll:         list.New(),
+		cache:      make(map[interface{}]*list.Element),
+	}
+}
+
+// 增加缓存对像到缓存中
+func (c *Cache) Add(key Key, value Danmu) {
+	if c.cache == nil {
+		c.cache = make(map[interface{}]*list.Element)
+		c.ll = list.New()
+	}
+	if ee, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ee)
+		ee.Value.(*entry).value = value
+		return
+	}
+	ele := c.ll.PushFront(&entry{key, value})
+	c.cache[key] = ele
+	if c.MaxEntries != 0 && c.ll.Len() > c.MaxEntries {
+		c.RemoveOldest()
+	}
+}
+
+// 从缓存中查找并取得键的值
+func (c *Cache) Get(key Key) (value Danmu, ok bool) {
+	if c.cache == nil {
+		return
+	}
+	if ele, hit := c.cache[key]; hit {
+		c.ll.MoveToFront(ele)
+		return ele.Value.(*entry).value, true
+	}
+	return
+}
+
+// 从缓存中删除给定键
+func (c *Cache) Remove(key Key) {
+	if c.cache == nil {
+		return
+	}
+	if ele, hit := c.cache[key]; hit {
+		c.removeElement(ele)
+	}
+}
+
+// RemoveOldest removes the oldest item from the cache.
+func (c *Cache) RemoveOldest() {
+	if c.cache == nil {
+		return
+	}
+	ele := c.ll.Back()
+	if ele != nil {
+		c.removeElement(ele)
+	}
+}
+
+func (c *Cache) removeElement(e *list.Element) {
+	c.ll.Remove(e)
+	kv := e.Value.(*entry)
+	delete(c.cache, kv.key)
+	if c.OnEvicted != nil {
+		c.OnEvicted(kv.key, kv.value)
+	}
+}
+
+// Len returns the number of items in the cache.
+func (c *Cache) Len() int {
+	if c.cache == nil {
+		return 0
+	}
+	return c.ll.Len()
+}
