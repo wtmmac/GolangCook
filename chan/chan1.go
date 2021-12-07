0efab79f0eec8d369363ded4791d8a4a79ed54c3
tony <wtmmac@gmail.com>
2021-12-07 17:28:24 +0800

    更新方法名

--- chan/chan1.go
+++ chan/chan1.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 var c = make(chan bool)
 var str1 string
 
-func f() {
-	str1 = "f() => hello, world"
+func chan1() {
+	str1 = "chan1() => hello, world"
 	fmt.Println(str1)
 	c <- true
 }
 
 func main() {
-	go f()
+	go chan1()
 	_ = <-c
-	for i := 0; i < 2; i++ {
-		time.Sleep(1 * time.Second)
-		fmt.Println("sleep")
-	}
+	//for i := 0; i < 2; i++ {
+	//	time.Sleep(1 * time.Second)
+	//	fmt.Println("sleep")
+	//}
 	fmt.Println("main() => Hello World!")
 }
