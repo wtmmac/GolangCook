aff7289ac7a88db0448f4b490009b0c9627591c8
tony <wtm>
2021-12-26 20:57:53 +0800

    up

--- gorm/rawSql/rawSql.go
+++ gorm/rawSql/rawSql.go
@@ -1,18 +1,20 @@
 package main
+
 import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
 type Result struct {
 	ID   int64
 	Name string
 	Age  int
 }
 
-func main()  {
+func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	dsn := "root:root111111@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -24,15 +26,12 @@ func main()  {
 	db.Raw("SELECT id, name, age FROM user WHERE name = ?", "小刚").Scan(&result)
 	fmt.Println(result)
 
+	fmt.Println("query all records")
 	// Raw SQL
 	rows, err := db.Raw("SELECT id, name, age FROM user").Rows()
 	defer rows.Close()
 	for rows.Next() {
-		db.ScanRows(rows,&result)
+		db.ScanRows(rows, &result)
 		fmt.Println(result.Name)
 	}
-
-
 }
-
-
