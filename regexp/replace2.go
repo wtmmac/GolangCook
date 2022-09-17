9f9d3be0a022ac06e7b4dd88332586dda88cd6b9
tony <wtm>
2022-09-17 16:10:56 +0800

    up

--- regexp/replace2.go
+++ regexp/replace2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 func main() {
@@ -17,4 +18,21 @@ func main() {
 	reg := regexp.MustCompile("\\s")
 	str = reg.ReplaceAllString(str, "")
 	fmt.Println(str)
+	test()
+}
+
+func test() {
+	str := `小明上街买菜
+买了1斤黄瓜花了5元
+买了2斤葡萄花了10.5元
+`
+	arr := strings.Split(str, "\n")
+	for _, line := range arr {
+		pattern := regexp.MustCompile(`(\d)斤(.*)花了(\d+(\.\d+)?)元`)
+		match := pattern.FindAllStringSubmatch(line, 10)
+		//fmt.Println(match)
+		if len(match) > 0 {
+			fmt.Println(match[0][1] + "," + match[0][2] + "," + match[0][3])
+		}
+	}
 }
