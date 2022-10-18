1a8a1306605d35cb5a66ea16201e27c93a368027
wtmmac <wtmmac@gmail.com>
2022-10-18 14:24:25 +0800

    test

--- time/time.go
+++ time/time.go
@@ -9,10 +9,11 @@ func main() {
 	fmt.Println("time.Now is:", time.Now())
 
 	start := time.Now()
-	time.Sleep(4 * time.Second)
+	time.Sleep(2 * time.Second)
 	t1 := time.Now()
 
 	spend_time := t1.Sub(start)
 
 	fmt.Printf("sleep cost: %v\n", spend_time)
+	fmt.Printf("spend_time TYPE: %T\n", start)
 }
