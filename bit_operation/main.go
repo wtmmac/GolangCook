0f7b8938c46d4b09e0463bf9f509b3702bf9e0d2
tony <wtm>
2023-04-18 21:18:31 +0800

    teste

---
+++ bit_operation/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	kindMask := (1 << 5) - 1
+	fmt.Println(kindMask)
+}
--- file/file.go
+++ file/file.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func file() {
+
+	f, err := os.Create("asdf.txt")
+	if err != nil {
+		log.Fatal("could not create file :", err)
+	}
+
+	defer func() {
+		_ = f.Close()
+	}()
 
 	fmt.Printf("asdf测试\n")
 }
