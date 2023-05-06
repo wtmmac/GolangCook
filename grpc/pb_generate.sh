adf6368cbf5654b34aaf876372dc09273460a782
wtmmac <wtmmac@gmail.com>
2023-05-06 14:24:03 +0800

    grpc

---
+++ grpc/pb_generate.sh
@@ -0,0 +1,4 @@
+# 生成方法2
+# Tips：将--plugin= 后边的 protoc-gen-go 和 protoc-gen-go-rpc 的前缀路径换成自己实际的地址
+# protoc --go-grpc_opt=require_unimplemented_servers=false 设置一个选项来生成没有向前兼容的代码
+protoc -I=./ ./api/*.proto --plugin=$(which protoc-gen-go) --go_out=./go_out --plugin=$(which protoc-gen-go-grpc) --go-grpc_out=./go_out
