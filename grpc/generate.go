c19273a3092062c75d5f271a504cc222ea73f92a
wtmmac <wtmmac@gmail.com>
2023-05-06 14:26:43 +0800

    grpc

--- grpc/generate.go
+++ grpc/generate.go
@@ -1,5 +1,5 @@
 package grpc
 
 // 生成方法1
-////go:generate protoc -I. -I$GOPATH/src --go_out=plugins=grpc:. --go_opt=paths=source_relative logic/logic.proto
+// go generate
 //go:generate protoc -I ./api/ --go_out=plugins=grpc:./api/ --go_opt=paths=source_relative ./api/hello.proto
