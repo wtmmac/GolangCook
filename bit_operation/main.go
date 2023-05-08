7bca147d58ffc069989232aca2e53eb069469907
wtmmac <wtmmac@gmail.com>
2023-05-08 16:23:26 +0800

    update

--- bit_operation/main.go
+++ bit_operation/main.go
@@ -5,4 +5,7 @@ import "fmt"
 func main() {
 	kindMask := (1 << 5) - 1
 	fmt.Println(kindMask)
+
+	const size = 64 << 10
+	fmt.Println(size)
 }
--- slice/slice1/main.go
+++ slice/slice1/main.go
@@ -11,4 +11,23 @@ func main() {
 	keys := make([]byte, 0, len(d))
 
 	fmt.Println(len(keys))
+
+	urls := make(map[string]string, 3)
+	urls["baidu"] = "www.baidu.com"
+	urls["google"] = "www.google.com"
+	urls["csdn"] = "www.csdn.net"
+
+	// bad method
+	names := make([]string, len(urls))
+	for key, _ := range urls {
+		names = append(names, key)
+	}
+	fmt.Printf("%#v ,%d\n", names, len(names))
+
+	// good method
+	namesGood := make([]string, 0)
+	for key, _ := range urls {
+		namesGood = append(namesGood, key)
+	}
+	fmt.Printf("%#v ,%d\n", namesGood, len(namesGood))
 }
