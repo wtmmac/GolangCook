42de340b0e1b9e2e9106f2b9f38c767ce51ee95d
wtmmac <wtmmac@gmail.com>
2023-11-24 10:39:52 +0800

    new files

---
+++ TDD/arrays/sum_test.go
@@ -0,0 +1,17 @@
+package arrays
+
+import (
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("collections of any size:", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d want %d give, %v", got, want, numbers)
+		}
+	})
+}
