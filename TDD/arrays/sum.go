f6111c0c9a6040e1131841601442ec78b0ad58f3
wtmmac <wtmmac@gmail.com>
2023-11-27 15:00:41 +0800

    sum arrays

--- TDD/arrays/sum.go
+++ TDD/arrays/sum.go
@@ -7,3 +7,11 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var result []int
+	for _, numbers := range numbersToSum {
+		result = append(result, Sum(numbers))
+	}
+	return result
+}
--- TDD/arrays/sum_test.go
+++ TDD/arrays/sum_test.go
@@ -1,17 +1,25 @@
 package arrays
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
 	t.Run("collections of any size:", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
-
 		got := Sum(numbers)
 		want := 6
 		if got != want {
 			t.Errorf("got %d want %d give, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9}, []int{2, 3})
+	want := []int{3, 9, 5}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
