8689d78d1241805e4fc36def05588d374a5744af
wtmmac <wtmmac@gmail.com>
2023-11-28 16:00:08 +0800

    refactor shape

--- TDD/structs/shapes.go
+++ TDD/structs/shapes.go
@@ -1,5 +1,32 @@
 package shapes
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
 func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
+}
+
+func Area(width, height float64) float64 {
+	return width * height
 }
--- TDD/structs/shapes_test.go
+++ TDD/structs/shapes_test.go
@@ -1,6 +1,8 @@
 package shapes
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	got := Perimeter(3.0, 5.0)
@@ -8,4 +10,23 @@ func TestPerimeter(t *testing.T) {
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
+}
+
+func TestArea(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+	t.Run("rectangles", func(t *testing.T) {
+		r := Rectangle{12.0, 6.0}
+		checkArea(t, r, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		shape := Circle{10.0}
+		checkArea(t, shape, 314.1592653589793)
+	})
 }
