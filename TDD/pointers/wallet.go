a4112a7823c6d3463d31c8acb04ba02f1f71491e
wtmmac <wtmmac@gmail.com>
2023-11-29 18:58:06 +0800

    Bitcoin wallet

--- TDD/pointers/wallet.go
+++ TDD/pointers/wallet.go
@@ -4,12 +4,16 @@ import "fmt"
 
 type Bitcoin int
 
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
 	balance Bitcoin
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	// fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
--- TDD/pointers/wallet_test.go
+++ TDD/pointers/wallet_test.go
@@ -1,17 +1,18 @@
 package pointers
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-	want := Bitcoin(10)
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+	t.Run("BitcoinWallet", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+		// fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+		want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
