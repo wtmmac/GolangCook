ba4e86c4ce7e1bf75e5ae86a08592bae3dcd4d82
wtmmac <wtmmac@gmail.com>
2023-11-29 14:46:31 +0800

    Table driven tests

--- TDD/structs/shapes_test.go
+++ TDD/structs/shapes_test.go
@@ -30,3 +30,23 @@ func TestArea(t *testing.T) {
 		checkArea(t, shape, 314.1592653589793)
 	})
 }
+
+func TestAreaWithTable(t *testing.T) {
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+	for _, v := range areaTests {
+		t.Run(v.name, func(t *testing.T) {
+			got := v.shape.Area()
+			if got != v.want {
+				t.Errorf("%#v got %g want %g", v.shape, got, v.want)
+			}
+		})
+	}
+}
