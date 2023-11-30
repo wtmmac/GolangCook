09e833c7421942b78152b7722381814fbbf06dc2
wtmmac <wtmmac@gmail.com>
2023-11-30 15:55:11 +0800

    creates base code

--- TDD/maps/dictionary.go
+++ TDD/maps/dictionary.go
@@ -6,6 +6,6 @@ func Search(dictionary map[string]string, keyword string) string {
 
 type Dictionary map[string]string
 
-func (d Dictionary) Search(keyword string) string {
-	return d[keyword]
+func (d Dictionary) Search(keyword string) (string, error) {
+	return d[keyword], nil
 }
