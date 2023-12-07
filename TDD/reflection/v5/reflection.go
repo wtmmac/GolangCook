88d0ea4e2a5f1012d5d04aef161078100efca806
wtmmac <wtmmac@gmail.com>
2023-12-07 17:09:22 +0800

    reflection version 5

---
+++ TDD/reflection/v5/reflection.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			fmt.Printf(("this a struct:%v"), field.Interface())
+			Walk(field.Interface(), fn)
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
