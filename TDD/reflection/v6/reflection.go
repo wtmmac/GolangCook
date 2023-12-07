2c4cedb686f8924d67058cb224fbaa5a2ea0d428
wtmmac <wtmmac@gmail.com>
2023-12-07 17:43:38 +0800

    refactor version 6

--- TDD/reflection/v6/reflection.go
+++ TDD/reflection/v6/reflection.go
@@ -1,30 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"reflect"
 )
 
 func Walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			Walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
 			Walk(val.Index(i).Interface(), fn)
 		}
-		return
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			fmt.Printf(("this a struct:%v"), field.Interface())
-			Walk(field.Interface(), fn)
-		}
+	case reflect.String:
+		fn(val.String())
 	}
 }
 
