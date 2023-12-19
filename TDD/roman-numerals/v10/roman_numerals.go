e86dd4c37e59a0cc8f3ff83e57d7351725d290c7
wtmmac <wtmmac@gmail.com>
2023-12-19 15:51:19 +0800

    refactor

--- TDD/roman-numerals/v10/roman_numerals.go
+++ TDD/roman-numerals/v10/roman_numerals.go
@@ -4,24 +4,28 @@ import (
 	"strings"
 )
 
-func ConverToArabic(roman string) int {
-	total := 0
-	for i := 0; i < len(roman); i++ {
-		symbol := roman[i]
-
-		if couldBeSubtractive(i, symbol, roman) {
-			value := allRomanNumerals.ValueOf(symbol, roman[i+1])
-			if value != 0 {
-				total += value
-				i++
-			} else {
-				total += allRomanNumerals.ValueOf(symbol)
-			}
-		} else {
-			total += allRomanNumerals.ValueOf(symbol)
-		}
+func ConverToArabic(roman string) (total int) {
+	for _, symbols := range windowedRoman(roman).Symbols() {
+		total += allRomanNumerals.ValueOf(symbols...)
 	}
-	return total
+	return
+	// total := 0
+	// for i := 0; i < len(roman); i++ {
+	// 	symbol := roman[i]
+
+	// 	if couldBeSubtractive(i, symbol, roman) {
+	// 		value := allRomanNumerals.ValueOf(symbol, roman[i+1])
+	// 		if value != 0 {
+	// 			total += value
+	// 			i++
+	// 		} else {
+	// 			total += allRomanNumerals.ValueOf(symbol)
+	// 		}
+	// 	} else {
+	// 		total += allRomanNumerals.ValueOf(symbol)
+	// 	}
+	// }
+	// return total
 }
 
 func couldBeSubtractive(index int, currentSymbol byte, roman string) bool {
@@ -58,6 +62,16 @@ func (r romanNumerals) ValueOf(symbols ...byte) int {
 	return 0
 }
 
+func (r romanNumerals) Exists(symbols ...byte) bool {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return true
+		}
+	}
+	return false
+}
+
 var allRomanNumerals = romanNumerals{
 	{1000, "M"},
 	{900, "CM"},
@@ -72,4 +86,25 @@ var allRomanNumerals = romanNumerals{
 	{5, "V"},
 	{4, "IV"},
 	{1, "I"},
+}
+
+type windowedRoman string
+
+func (w windowedRoman) Symbols() (symbols [][]byte) {
+	for i := 0; i < len(w); i++ {
+		symbol := w[i]
+		notAtEnd := i+1 < len(w)
+
+		if notAtEnd && isSubtractive(symbol) && allRomanNumerals.Exists(symbol, w[i+1]) {
+			symbols = append(symbols, []byte{symbol, w[i+1]})
+			i++
+		} else {
+			symbols = append(symbols, []byte{symbol})
+		}
+	}
+	return
+}
+
+func isSubtractive(symbol uint8) bool {
+	return symbol == 'I' || symbol == 'X' || symbol == 'C'
 }
