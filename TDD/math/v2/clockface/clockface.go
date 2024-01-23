76cfa9c3574638cd6216b00c6ab3be262af37d4b
wtmmac <wtmmac@gmail.com>
2024-01-23 14:31:29 +0800

    version 2

---
+++ TDD/math/v2/clockface/clockface.go
@@ -0,0 +1,19 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi
+}
