27d91cfa5779d158c7dcc0f234bbd8f71418e665
wtmmac <wtmmac@gmail.com>
2024-01-23 16:41:06 +0800

    vresion 5:SecondHand is ok

---
+++ TDD/math/v5/clockface/clockface.go
@@ -0,0 +1,31 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func SecondHand(t time.Time) Point {
+	p := secondHandPoint(t)
+	p = Point{p.X * 90, p.Y * 90}   // scale
+	p = Point{p.X, -p.Y}            // flip
+	p = Point{p.X + 150, p.Y + 150} // translate
+	return p
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi / (30 / float64(t.Second()))
+}
+
+func secondHandPoint(time time.Time) Point {
+	angle := secondsInRadians(time)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
