37348f4ae3e855b8c4825533cf95257d89da635a
wtmmac <wtmmac@gmail.com>
2024-02-27 15:32:16 +0800

    SVG elements

--- TDD/math/v8/clockface/clockface_acceptance_test.go
+++ TDD/math/v8/clockface/clockface_acceptance_test.go
@@ -1,12 +1,38 @@
 package clockface_test
 
 import (
+	"encoding/xml"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/wtmmac/GolangCook/TDD/math/v8/clockface"
 )
+
+type SVG struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  Circle   `xml:"circle"`
+	Line    []Line   `xml:"line"`
+}
+
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
+}
+
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
 
 func TestSVGWriterAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
