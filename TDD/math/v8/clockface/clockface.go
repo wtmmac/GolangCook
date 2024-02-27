cca9a24d30ed25751b5b8b6db7eda1594cf7d158
wtmmac <wtmmac@gmail.com>
2024-02-27 15:19:52 +0800

    V8

---
+++ TDD/math/v8/clockface/clockface.go
@@ -0,0 +1,36 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+// func SecondHand(t time.Time) Point {
+// 	p := secondHandPoint(t)
+// 	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+// 	p = Point{p.X, -p.Y}                                      // flip
+// 	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // translate
+// 	return p
+// }
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi / (30 / float64(t.Second()))
+}
+
+func secondHandPoint(time time.Time) Point {
+	angle := secondsInRadians(time)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
+
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) +
+		(math.Pi / (30 / float64(t.Minute())))
+}
---
+++ TDD/math/v8/clockface/clockface_test.go
@@ -0,0 +1,64 @@
+package clockface
+
+import (
+	"math"
+	"testing"
+	"time"
+)
+
+func Test_secondsInRadians(t *testing.T) {
+	type args struct {
+		t time.Time
+	}
+	tests := []struct {
+		args args
+		want float64
+	}{
+		{args{simpleTime(0, 0, 30)}, math.Pi},
+		{args{t: simpleTime(0, 0, 0)}, 0},
+		{args{t: simpleTime(0, 0, 45)}, math.Pi / 2 * 3},
+		{args{t: simpleTime(0, 0, 7)}, math.Pi / 30 * 7},
+	}
+	for _, tt := range tests {
+		t.Run(testName(tt.args.t), func(t *testing.T) {
+			if got := secondsInRadians(tt.args.t); got != tt.want {
+				t.Errorf("secondsInRadians() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSecondHandVector(t *testing.T) {
+	type args struct {
+		t time.Time
+	}
+	tests := []struct {
+		args  args
+		point Point
+	}{
+		{args{simpleTime(0, 0, 30)}, Point{0, -1}},
+		{args{simpleTime(0, 0, 45)}, Point{-1, 0.00000001}},
+	}
+	for _, tt := range tests {
+		t.Run(testName(tt.args.t), func(t *testing.T) {
+			if got := secondHandPoint(tt.args.t); !roughlyEqualPoint(got, tt.point) {
+				t.Errorf("secondHandPoint() = %v, want %v", got, tt.point)
+			}
+		})
+	}
+}
+func roughlyEqualFloat64(a, b float64) bool {
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+}
+
+func roughlyEqualPoint(a, b Point) bool {
+	return roughlyEqualFloat64(a.X, b.X) &&
+		roughlyEqualFloat64(a.Y, b.Y)
+}
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
