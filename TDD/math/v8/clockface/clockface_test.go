060f64b0c09f07f77c03f347f5c3a4f19743084e
wtmmac <wtmmac@gmail.com>
2024-02-27 16:49:56 +0800

    ok

--- TDD/math/v8/clockface/clockface_test.go
+++ TDD/math/v8/clockface/clockface_test.go
@@ -37,7 +37,7 @@ func TestSecondHandVector(t *testing.T) {
 		point Point
 	}{
 		{args{simpleTime(0, 0, 30)}, Point{0, -1}},
-		{args{simpleTime(0, 0, 45)}, Point{-1, 0.00000001}},
+		{args{simpleTime(0, 0, 45)}, Point{-1, 0}},
 	}
 	for _, tt := range tests {
 		t.Run(testName(tt.args.t), func(t *testing.T) {
@@ -47,6 +47,25 @@ func TestSecondHandVector(t *testing.T) {
 		})
 	}
 }
+
+func TestMinutesInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 30, 0), math.Pi},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := minutesInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+}
+
 func roughlyEqualFloat64(a, b float64) bool {
 	const equalityThreshold = 1e-7
 	return math.Abs(a-b) < equalityThreshold
