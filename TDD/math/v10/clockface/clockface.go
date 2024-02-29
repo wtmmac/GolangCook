e8f3e4f69c20949f02e10fb1a652482b04ae3cc0
wtmmac <wtmmac@gmail.com>
2024-02-29 15:59:06 +0800

    v10

---
+++ TDD/math/v10/clockface/clockface.go
@@ -0,0 +1,52 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+type Point struct {
+	X float64 `json:"x,omitempty"`
+	Y float64 `json:"y,omitempty"`
+}
+
+// func SecondHand(t time.Time) Point {
+// 	p := secondHandPoint(t)
+// 	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+// 	p = Point{p.X, -p.Y}                                      // flip
+// 	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // translate
+// 	return p
+// }
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi / (30 / float64(t.Second()))
+}
+
+func secondHandPoint(t time.Time) Point {
+	return angleToPoint(secondsInRadians(t))
+}
+
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) +
+		(math.Pi / (30 / float64(t.Minute())))
+}
+
+func minuteHandPoint(t time.Time) Point {
+	return angleToPoint(minutesInRadians(t))
+}
+
+func hoursInRadians(t time.Time) float64 {
+	// return (math.Pi / (6 / float64(t.Hour())))
+	return (minutesInRadians(t) / 12) + (math.Pi / (6 / (float64(t.Hour() % 12))))
+}
+
+func hourHandPoint(t time.Time) Point {
+	return angleToPoint(hoursInRadians(t))
+}
+
+func angleToPoint(angle float64) Point {
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
