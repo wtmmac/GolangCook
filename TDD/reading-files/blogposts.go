4ec51fd992210d4ff4a84dd654edbd5937986d73
wtmmac <wtmmac@gmail.com>
2024-03-12 16:13:39 +0800

    getPost

--- TDD/reading-files/blogposts.go
+++ TDD/reading-files/blogposts.go
@@ -1,6 +1,7 @@
 package blogposts
 
 import (
+	"io"
 	"io/fs"
 	"testing/fstest"
 )
@@ -11,8 +12,29 @@ func NewPostsFromFS(fileSystem fstest.MapFS) ([]Post, error) {
 		return nil, err
 	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	var post = Post{Title: string(postData)[7:]}
+
+	return post, nil
 }
--- TDD/reading-files/blogposts_test.go
+++ TDD/reading-files/blogposts_test.go
@@ -8,6 +8,23 @@ import (
 	blogposts "github.com/wtmmac/GolangCook/TDD/reading-files"
 )
 
+func TestNewBlogPostsFirst(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
+	}
+	posts, err := blogposts.NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var got = posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
+
 func TestNewBlogPosts(t *testing.T) {
 	const (
 		firstBody = `Title: Post 1
