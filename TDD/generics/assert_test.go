380c9762353c2fc454cd0d48bb9b0ef5fefb1238
tony <wtm>
2024-03-28 22:02:06 +0800

    finished the test

--- TDD/generics/assert_test.go
+++ TDD/generics/assert_test.go
@@ -32,6 +32,29 @@ func TestStack(t *testing.T) {
 		AssertEqual(t, value, 123)
 		AssertTure(t, myStackOfInts.IsEmpty())
 	})
+
+	t.Run("generics stack", func(t *testing.T) {
+		myStackOfInts := new(Stack[int])
+		// check stack is empty
+		AssertTure(t, myStackOfInts.IsEmpty())
+		// add a thing, then check it's not empty
+		myStackOfInts.Push(123)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+		// add another thing, pop it back again
+		myStackOfInts.Push(456)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 456)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 123)
+		AssertTure(t, myStackOfInts.IsEmpty())
+
+		// can get the numbers we put in as numbers, bot untyped
+		myStackOfInts.Push(1)
+		myStackOfInts.Push(2)
+		firstNum, _ := myStackOfInts.Pop()
+		secondNum, _ := myStackOfInts.Pop()
+		AssertEqual(t, firstNum+secondNum, 3)
+	})
 }
 
 func AssertTure(t *testing.T, got bool) {
