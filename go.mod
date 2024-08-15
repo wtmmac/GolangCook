d4781dc756e3ab07f787d40c76ed4e440a96b015
wtmmac <wtmmac@gmail.com>
2024-08-15 15:20:31 +0800

    test

--- go.mod
+++ go.mod
@@ -32,6 +32,7 @@ require (
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/devfeel/mapper v0.7.13
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
--- time/second/main.go
+++ time/second/main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/devfeel/mapper"
 )
 
 func main() {
 	t := time.Now()
 	fmt.Println(t.Format("15:04:05"))
 	fmt.Println(t.Second())
+	fmt.Println(t.Local().GoString())
+
+	var oldVipExpireTime time.Time
+	newVipExpireTime := time.Now().AddDate(0, 3, 0)
+	snapshot := make(map[string]interface{})
+	snapshot["donate"] = 1
+	snapshot["donate"] = 3
+	snapshot["oldVipExpireTime"] = oldVipExpireTime.Format("2006-01-02 15:04:05")
+	snapshot["newVipExpireTime"] = newVipExpireTime.Format("2006-01-02 15:04:05")
+	snapshotBytes, err := mapper.MapToJson(snapshot)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(snapshotBytes))
+
 }
