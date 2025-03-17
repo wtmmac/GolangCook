270a22fa3a501e5794e4cf1e8a357c9675edf5ce
wtmmac <wtmmac@gmail.com>
2025-03-17 10:27:42 +0800

    panic时recover打印更详细的Stack trace

--- panicRecover/panic.go
+++ panicRecover/panic.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"errors"
+	"context"
 	"fmt"
+	"log"
+	"runtime/debug"
 )
 
 func main() {
 	defer func() { //必须要先声明defer，否则不能捕获到panic异常
 		fmt.Println("enter defer func")
 		if err := recover(); err != nil {
-			fmt.Println(err)
+			fmt.Printf("Panic occurred: %v\n", err)
+			fmt.Printf("Stack trace:\n%s\n", debug.Stack())
 		}
 		fmt.Println("end of defer")
 	}()
@@ -18,6 +21,17 @@ func main() {
 
 func raisePanic() {
 	fmt.Println("before panic")
-	panic(errors.New("!!fatal error!"))
+	// panic(errors.New("!!fatal error!"))
+	raseError()
 	fmt.Println("after panic")
+}
+
+func raseError() {
+	InfoContextf(context.Background(), "hello")
+}
+
+func InfoContextf(ctx context.Context, format string, args ...interface{}) {
+	traceId := ctx.Value("TraceKey").(string)
+	prefix := fmt.Sprintf("INFO  %s ", traceId)
+	_ = log.Output(2, fmt.Sprintf(prefix+format+"\n", args...))
 }
